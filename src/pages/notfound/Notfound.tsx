5abf04fcc783ba45aee193b313e45f68cda029dd
manasamancharla <manasamancharla11@protonmail.com>
2025-01-10 13:28:10 +0530

    updated packages

--- src/pages/notfound/Notfound.tsx
+++ src/pages/notfound/Notfound.tsx
@@ -1,24 +1,31 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import Topbar from "../../components/topbar/Topbar";
 import Footer from "../../components/footer/Footer";
 
 import "./notfound.scss";
 
 const Notfound: React.FC = () => {
-  return (
-    <div className="notfound container">
-      <Topbar />
-      <div style={{ flexGrow: 1 }}></div>
-      <div className="notfound__wrapper">
-        <h1>Page not found</h1>
-        <p>Uh oh, we can't seem to find the page you're looking for.</p>
-        <button className="btn">Go to your feed</button>
-      </div>
-      <div style={{ flexGrow: 1 }}></div>
-      <Footer />
-    </div>
-  );
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate("/");
+	};
+	return (
+		<div className="notfound container">
+			<Topbar />
+			<div style={{ flexGrow: 1 }}></div>
+			<div className="notfound__wrapper">
+				<h1>Page not found</h1>
+				<p>Uh oh, we can't seem to find the page you're looking for.</p>
+				<button className="btn" onClick={handleClick}>
+					Go to your feed
+				</button>
+			</div>
+			<div style={{ flexGrow: 1 }}></div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Notfound;
